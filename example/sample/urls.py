396cf76d645811824f5570a545ba4c4504840ccd
pclumson1 <37989010+pclumson1@users.noreply.github.com>
2021-02-15 04:17:18 -0800

    Update urls.py
    Update sample urls to path instead.

--- example/sample/urls.py
+++ example/sample/urls.py
@@ -2,18 +2,17 @@
 from __future__ import unicode_literals
 
 #from django.conf.urls import include, url
-from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'sample.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    # url(r"^friendship/", include("friendship.urls")),
+    # url(r"^admin/", admin.site.urls), 
+
+    path('friendship', include('friendship.urls')),
+    path('admin', include('admin.site.urls')),
     
-    #url(r"^friendship/", include("friendship.urls")),
-    #url(r"^admin/", admin.site.urls),
-    
-    path("admin/", include("admin.urls")),
-    path("frienship/", include("friendship.urls")),
-   
 ]
